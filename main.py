7944801a04afcc3d8a633d7eb8293aef20b9121e
AlmeidaLevi <almeidalevi878@gmail.com>
2023-12-25 20:13:59 -0300

    Bot whatsapp

--- main.py
+++ main.py
@@ -5,8 +5,7 @@ from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.common.exceptions import (WebDriverException,
                                         NoSuchElementException,
-                                        NoSuchWindowException,
-                                        StaleElementReferenceException)
+                                        NoSuchWindowException)
 from chromedriver_py import binary_path
 import time
 
@@ -16,47 +15,34 @@ options = webdriver.ChromeOptions()
 browser = webdriver.Chrome(service=service, options=options)
 
 browser.get("https://web.whatsapp.com/")
-break_loop = False
-levi_alcancado = False
-programado_alcancado = False
 
 time.sleep(20)
 
 while True:
     cells = []
     title = ""
+    message = "Essa mensagem foi enviado por um bot automatizado com \
+selenium, para mais detalhes acesse: \
+https://github.com/LevideAlmeida/Bot_Whatsapp"
 
-    time.sleep(3)
     try:
         windows = browser.window_handles
     except WebDriverException:
         break
 
     try:
-        cells = browser.find_elements(By.TAG_NAME, "span")
-    except NoSuchWindowException:
-        print("nao achou janela")
-        cells = []
-    except NoSuchElementException:
-        print("nao achou o elemento")
-        cells = []
+        cells = browser.find_elements(By.CLASS_NAME, "_2H6nH")
     except NoSuchWindowException:
         break
+    except NoSuchElementException:
+        time.sleep(2)
 
-    for cell in cells:
-        try:
-            title = cell.get_attribute("title")
-        except StaleElementReferenceException:
-            title = ""
-        except NoSuchElementException:
-            title = ""
-        except NoSuchWindowException:
-            break
-
-        if title == "+55 85 8686-2996" and not levi_alcancado:
-            print("Deu bom", title)
+    try:
+        for cell in cells:
             cell.click()
             input_box = browser.find_element(By.CLASS_NAME, "_3Uu1_")
-            input_box.send_keys("Feliz Natal")
+            input_box.send_keys(message)
             input_box.send_keys(Keys.ENTER)
-            levi_alcancado = True
+            time.sleep(10)
+    except NoSuchWindowException:
+        break
